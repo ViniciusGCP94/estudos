922238ea3d86863b594ed24f8a8cbb4e9be48405
ViniciusGCP94 <vinniegcp@gmail.com>
2025-07-17 20:32:00 -0300

    Organizando boilerplate e estrutura - To Do List

--- AVANTI-innovation-class/React/todolist/src/components/ToDoList.jsx
+++ AVANTI-innovation-class/React/todolist/src/components/ToDoList.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import styles from './ToDoList.module.css'
 
 const ToDoList = () => {
   return (
-    <div>ToDoList</div>
+    <div className={styles.container}>
+        <h2 className={styles.title}>Lista de Tarefas</h2>
+        <div className={styles.inputContent}>
+            <input 
+                type='text'
+                placeholder='Adicione uma tarefa'
+                className={styles.input}
+            />
+            <button className={styles.button}>Adicionar Tarefa</button>
+        </div>
+        <ul className={styles.taskList}>
+          <li className={styles.taskListItem}>Tarefa</li>
+        </ul>
+    </div>
   )
 }
 
