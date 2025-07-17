c2f63e55fb301e08f5a9bf2874cd2048444224cc
ViniciusGCP94 <vinniegcp@gmail.com>
2025-07-17 20:58:00 -0300

    Controlando estado do input e das tarefas - ToDoList

--- AVANTI-innovation-class/React/todolist/src/components/ToDoList.jsx
+++ AVANTI-innovation-class/React/todolist/src/components/ToDoList.jsx
@@ -2,22 +2,34 @@ import React from 'react'
 import styles from './ToDoList.module.css'
 
 const ToDoList = () => {
-  return (
-    <div className={styles.container}>
-        <h2 className={styles.title}>Lista de Tarefas</h2>
-        <div className={styles.inputContent}>
-            <input 
-                type='text'
-                placeholder='Adicione uma tarefa'
-                className={styles.input}
-            />
-            <button className={styles.button}>Adicionar Tarefa</button>
-        </div>
-        <ul className={styles.taskList}>
-          <li className={styles.taskListItem}>Tarefa</li>
-        </ul>
-    </div>
-  )
+    const [task, setTask] = React.useState('');
+    const [tasks, setTasks] = React.useState([]);
+
+    const addTask = () => {
+      if (task.trim() === '') return;
+      setTasks([...tasks, task]);
+      setTask('');
+    }
+
+
+    return (
+      <div className={styles.container}>
+          <h2 className={styles.title}>Lista de Tarefas</h2>
+          <div className={styles.inputContent}>
+              <input 
+                  type='text'
+                  placeholder='Adicione uma tarefa'
+                  className={styles.input}
+                  value={task}
+                  onChange={(e) => setTask(e.target.value)}
+              />
+              <button onClick={addTask} className={styles.button}>Adicionar Tarefa</button>
+          </div>
+          <ul className={styles.taskList}>
+            <li className={styles.taskListItem}>Tarefa</li>
+          </ul>
+      </div>
+    )
 }
 
 export default ToDoList
